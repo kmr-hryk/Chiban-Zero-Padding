7ec1c10aab78d7407e3f1ca35b069cf491a97d03
Hiroyuki Kimura <z_kimura@k-side.net>
2015-01-15 09:43:47 +0900

    コマンドレットとして使用できるように関数の中に入れた

--- Chiban-Zero-Padding.ps1
+++ Chiban-Zero-Padding.ps1
@@ -1,23 +1,25 @@
-function Zero-Padding() {
-   $str = [String]$ARGS[0]
-   $length = $ARGS[1]
+function Chiban-Zero-Padding {
+   function Zero-Padding {
+      $str = [String]$ARGS[0]
+      $length = $ARGS[1]
 
-   While ( $str.Length -lt $length ) {
-      $str = "0$str"
+      While ( $str.Length -lt $length ) {
+         $str = "0$str"
+      }
+
+      Return $str
    }
 
-   Return $str
+   $chiban = [String]$ARGS[0] -Split("-")
+
+   $oya = Zero-Padding $chiban[0] 4
+   if ( $chiban.Length -eq 2 ) {
+      $eda = Zero-Padding $chiban[1] 3
+   } else {
+      $eda = "000"
+   }
+
+   $chiban = "$oya-$eda"
+
+   Write-Output $chiban
 }
-
-$chiban = [String]$ARGS[0] -Split("-")
-
-$oya = Zero-Padding $chiban[0] 4
-if ( $chiban.Length -eq 2 ) {
-   $eda = Zero-Padding $chiban[1] 3
-} else {
-   $eda = "000"
-}
-
-$chiban = "$oya-$eda"
-
-Write-Output $chiban
